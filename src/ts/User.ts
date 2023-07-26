ea3f160896ec0db48ee36a345acbb1eaf942c21f
Miras <bking4234@gmail.com>
2023-07-26 10:46:47 +0600

    add: add ts file class User

--- src/ts/User.ts
+++ src/ts/User.ts
@@ -1,3 +1,16 @@
-class User{
-    
+import {faker} from '@faker-js/faker';
+
+export class User{
+    name: string;
+    location:{
+        lat:number;
+        lng:number;
+    }
+    constructor(){
+        this.name = faker.person.firstName();
+        this.location = {
+            lat: faker.location.latitude(),
+            lng: faker.location.longitude(),
+        }
+    } 
 }
